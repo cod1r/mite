367540de09189b5558dfc6da8f12c32d251a6caf
cod1r <jasonho353@gmail.com>
2023-02-02 23:31:13 -0600

    iced events

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use iced::widget::{column, text_input};
-use iced::{Alignment, Element, Sandbox, Settings};
+use iced::{executor, keyboard};
+use iced::{Alignment, Application, Command, Element, Settings, Subscription, Theme};
+use iced_native::Event;
 
 pub fn main() -> iced::Result {
     ChatBox::run(Settings::default())
@@ -12,30 +14,55 @@ struct ChatBox {
 #[derive(Debug, Clone)]
 enum Message {
     OnType(String),
+    Ev(Event),
 }
 
-impl Sandbox for ChatBox {
+impl Application for ChatBox {
     type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self { value: "".to_string() }
+    fn new(_flags: ()) -> (ChatBox, Command<Message>) {
+        (
+            ChatBox {
+                value: "".to_string(),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
-        String::from("Counter - Iced")
+        String::from("mite")
     }
 
-    fn update(&mut self, message: Message) {
-        let Message::OnType(s) = message;
-        self.value = s;
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::OnType(s) => {
+                self.value = s;
+                Command::none()
+            }
+            Message::Ev(e) => {
+                match e {
+                    Event::Keyboard(keyboard::Event::CharacterReceived(k)) => {
+                        if k as u8 == 13 {
+                            println!("ENTER");
+                        }
+                    }
+                    _ => {}
+                }
+                Command::none()
+            }
+        }
+    }
+    fn subscription(&self) -> Subscription<Message> {
+        iced_native::subscription::events().map(Message::Ev)
     }
 
     fn view(&self) -> Element<Message> {
-        column![
-            text_input("", &self.value, Message::OnType),
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
+        column![text_input("", &self.value, Message::OnType),]
+            .padding(20)
+            .align_items(Alignment::Center)
+            .into()
     }
 }
